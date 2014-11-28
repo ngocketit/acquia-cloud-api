68b1a4df7d41e2131698ca117238c5232b914633
Phi Van Ngoc <phi.vanngoc@activearkjwt.com>
2014-11-28 11:35:55 +0200

    Add checking for validity of credentials & passing command params from arguments

--- acquia-cloud.sh
+++ acquia-cloud.sh
@@ -10,7 +10,7 @@ INSTALL_PATH=/usr/local/bin/acquia-cloud-util
 CREDENTIALS_CACHE_DIR=$HOME/.acquia-cloud-util
 DEFAULT_EMAIL_ADDRESS=drupal@activeark.com
 API_ENDPOINT=https://cloudapi.acquia.com/v1
-COMMAND_FILE_PATH=./acquia-commands.sh
+COMMAND_FILE_PATH=./acquia-commands.txt
 DEFAULT_SITE_NAME=default
 
 CHOSEN_CACHE_FILE=""
@@ -313,11 +313,6 @@ __add_credentials()
 
 __get_environment()
 {
-  __select_environment
-}
-
-__select_environment()
-{
   local environments=(dev stg prod) choice
   select choice in "${environments[@]}"; do
     case $REPLY in
@@ -336,7 +331,8 @@ __select_environment()
 __parse_commands_file()
 {
   [ ! -f "$COMMAND_FILE_PATH" ] && __print_error "Command file not found" && exit 1
-  local line category old_ifs=$IFS
+  local line category old_ifs=$IFS verbose=""
+  [ "$1" == "verbose" ] && verbose="verbose"
 
   while read line; do
     [ -z "$line" ] && continue
@@ -345,10 +341,11 @@ __parse_commands_file()
     if [ ! -z "$category" ]; then
       category=${category/#[/}
       category=${category/%]/}
-      echo -e "\n$(__to_uppercase $category)"
+      [ ! -z "$verbose" ] && echo -e "\n$(__to_uppercase $category)" && echo '-----------------------------------------------------'
     else
       IFS=$';'
       __parse_command_line $line
+      [ ! -z "$verbose" ] && echo -e "$COMMAND_INDEX) ${txtYellow}${COMMAND_NAMES[$COMMAND_INDEX]}:${txtOff} ${COMMAND_DESCS[$COMMAND_INDEX]}"
       let "COMMAND_INDEX+=1"
     fi
   done < $COMMAND_FILE_PATH
@@ -363,7 +360,6 @@ __parse_command_line()
   COMMAND_METHODS[$COMMAND_INDEX]="$cmd_method"
   COMMAND_DESCS[$COMMAND_INDEX]="$cmd_desc"
   COMMAND_PATHS[$COMMAND_INDEX]="$cmd_path"
-  echo -e "$COMMAND_INDEX) ${txtYellow}$cmd_name:${txtOff} $cmd_desc"
 }
 
 __get_command_index_from_name()
@@ -425,16 +421,20 @@ __get_site_realm()
   echo "prod:$SITE_NAME"
 }
 
+__init_command_path()
+{
+  PREPARED_COMMAND_PATH=${COMMAND_PATHS[$COMMAND_INDEX]}
+}
+
 __get_command()
 {
-  __parse_commands_file
 
   COMMAND_INDEX=0
   local command_id=""
-  __print_prompt "Enter the command number or command name:" && read command_id
+  __print_prompt "\nEnter the command number or command name:" && read command_id
   [ ! -z "$(__is_integer $command_id)" ] && COMMAND_INDEX=$command_id || COMMAND_INDEX=$(__get_command_index_from_name $command_id)
   [ $COMMAND_INDEX -lt 1 ] || [ $COMMAND_INDEX -gt ${#COMMAND_NAMES[@]} ] && __print_error "Invalid command number or name. Aborted!" && exit 1
-  PREPARED_COMMAND_PATH=${COMMAND_PATHS[$COMMAND_INDEX]}
+  __init_command_path
 }
 
 __ensure_command_params()
@@ -511,18 +511,25 @@ __replace_command_pattern()
   PREPARED_COMMAND_PATH=${PREPARED_COMMAND_PATH/$1/$2}
 }
 
+__issue_curl_command()
+{
+  echo "curl -s -u $EMAIL_ADDRESS:$PRIVATE_KEY -X ${1} ${API_ENDPOINT}${2}.json" > /tmp/command.txt
+  echo $(curl -s -u $EMAIL_ADDRESS:$PRIVATE_KEY -X ${1} ${API_ENDPOINT}${2}.json)
+}
+
 __check_async_task_state()
 {
   local cmd_name="task-info"
   local cmd_index=$(__get_command_index_from_name "$cmd_name")
   local cmd_path=${COMMAND_PATHS[$cmd_index]}
+  local cmd_method=${COMMAND_METHODS[$cmd_index]}
 
   cmd_path=${cmd_path/:site/$(__get_site_realm)}
   cmd_path=${cmd_path/:task/$1}
   local state="" cmd_output="" local delay=0.5
 
   while [ 1 ]; do
-    cmd_output=$(curl -s -u $EMAIL_ADDRESS:$PRIVATE_KEY -X ${COMMAND_METHODS[$cmd_index]} ${API_ENDPOINT}${cmd_path}.json)
+    cmd_output=$(__issue_curl_command $cmd_method $cmd_path)
     state=$(__parse_json "$cmd_output" state)
     echo "State: $state"
     [ "$state" == "done" -o "$state" == "error" ] && break
@@ -535,60 +542,112 @@ __execute_command()
   echo "${COMMAND_DESCS[$COMMAND_INDEX]}"
   local old_ifs=$IFS
   IFS=$'/'
-  __ensure_command_params ${COMMAND_PATHS[$COMMAND_INDEX]}
+  __ensure_command_params $PREPARED_COMMAND_PATH
   IFS=$old_ifs
 
-  local cmd_output=$(curl -s -u $EMAIL_ADDRESS:$PRIVATE_KEY -X ${COMMAND_METHODS[$COMMAND_INDEX]} ${API_ENDPOINT}${PREPARED_COMMAND_PATH}.json)
+  local cmd_output=$(__issue_curl_command ${COMMAND_METHODS[$COMMAND_INDEX]} $PREPARED_COMMAND_PATH)
   __beautify_json_output "$cmd_output"
   __wait_for_async_task "$cmd_output"
 }
 
-__get_options()
+__dry_run_credentials_check()
 {
-  [ $# -eq 2 ] && SITE_NAME=$1 && COMMAND=$2
+  local cmd_name="database-list"
+  local cmd_path='/sites/:site/dbs'
 
-  [ -z "$SITE_NAME" ] && __get_sitename
+  cmd_path=${cmd_path/:site/$(__get_site_realm)}
+  local cmd_output=$(__issue_curl_command "GET" "$cmd_path")
+  message=$(__parse_json "$cmd_output" message)
+
+  [ "$message" == "Not authorized" ] && EMAIL_ADDRESS="" && PRIVATE_KEY="" && __print_error "Default credentials failed for this site. Please enter new one"
+}
+
+__ensure_valid_credentials()
+{
   local cred_file=$(__get_credentials_file $SITE_NAME) default_cred_file=$(__get_credentials_file $DEFAULT_SITE_NAME)
 
-  [ -f "$cred_file" ] && source $cred_file 
-  [ -f "$default_cred_file" ] && source $default_cred_file
+  if [ -f "$cred_file" ]; then
+    source $cred_file
+  else
+    source $default_cred_file
+  fi
+
+  __dry_run_credentials_check
+}
+
+__get_command_params()
+{
+  local cmd_path=$1
+  local cmd_parts=${cmd_path//\// }
+  shift
+
+  # Ignore site name & command name which have been read already
+  [ $# -ge 2 ] && shift && shift
+
+  for breadcrumb in $cmd_parts; do
+    [ -z "$breadcrumb" ] || [ ! -z "$(echo $breadcrumb|grep -E -v "^:")" ] && continue
+    [ -z "$1" ] && break
+
+    case $breadcrumb in
+      :site)
+        __replace_command_pattern $breadcrumb $(__get_site_realm)
+        ;;
+
+      *)
+        __replace_command_pattern $breadcrumb $1 && shift
+        ;;
+    esac
+  done
+}
+
+__get_options()
+{
+  if [ $# -eq 1 ]; then
+    case "$1" in
+      --help|-help)
+        __print_usage
+        ;;
+
+      --update|-update)
+        __check_update verbose
+        ;;
+
+      --list-cred|-list)
+        __print_cached_credentials
+        ;;
+
+      --version|-version)
+        __print_current_version
+        ;;
+
+      --add-cred|-add)
+        __add_credentials
+        ;;
+
+      *) 
+        __print_usage
+        ;;
+    esac
+    exit 0
+  fi
+
+  [ $# -ge 2 ] && SITE_NAME=$1 && COMMAND=$2
+
+  [ -z "$SITE_NAME" ] && __get_sitename
+  __ensure_valid_credentials
 
   [ -z "$EMAIL_ADDRESS" ] && __get_email_address
   [ -z "$PRIVATE_KEY" ] && __get_private_key
 
-  [ ! -z "$COMMAND" ] && COMMAND_INDEX=$(__get_command_index_from_name $COMMAND)
-  [ -z "$COMMAND" ] && __get_command
+  if [ -z "$COMMAND" ]; then
+    __parse_commands_file verbose && __get_command
+  else
+    __parse_commands_file && COMMAND_INDEX=$(__get_command_index_from_name "$COMMAND") && __init_command_path
+  fi
+
+  __get_command_params $PREPARED_COMMAND_PATH "$@"
 }
 
-
-if [ $# -eq 1 ]; then
-  case "$1" in
-    --help|-help)
-      __print_usage
-      exit 0
-      ;;
-
-    --update|-update)
-      __check_update verbose
-      ;;
-
-    --list-cred|-list)
-      __print_cached_credentials
-      ;;
-
-    --version|-version)
-      __print_current_version
-      ;;
-
-    --add-cred|-add)
-      __add_credentials
-      ;;
-
-    *) 
-      ;;
-  esac
-else
-  __check_update
-  __get_options "$@"
-  __execute_command
-fi
+__check_update
+__get_options "$@"
+__execute_command
